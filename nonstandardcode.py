6e22f770f658b359967b8de021c5190d7ab9ece0
Danasekarane Arounachalam - TA <danasekarane.aro@tigeranalytics.com>
2024-04-22 17:00:25 +0530

    #3 MLE Assignment 01.3 Adding flake, precommit support

--- nonstandardcode.py
+++ nonstandardcode.py
@@ -46,7 +46,7 @@ train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
 
 housing['income_cat'] = pd.cut(
     housing['median_income'],
-    bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+    bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf],
     labels=[1, 2, 3, 4, 5],
 )
 
@@ -63,15 +63,19 @@ def income_cat_proportions(data):
 
 train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
 
-compare_props = pd.DataFrame({
-    'Overall': income_cat_proportions(housing),
-    'Stratified': income_cat_proportions(strat_test_set),
-    'Random': income_cat_proportions(test_set),
-}).sort_index()
-compare_props['Rand. %error'] = 100 * \
-    compare_props['Random'] / compare_props['Overall'] - 100
-compare_props['Strat. %error'] = 100 * \
-    compare_props['Stratified'] / compare_props['Overall'] - 100
+compare_props = pd.DataFrame(
+    {
+        'Overall': income_cat_proportions(housing),
+        'Stratified': income_cat_proportions(strat_test_set),
+        'Random': income_cat_proportions(test_set),
+    }
+).sort_index()
+compare_props['Rand. %error'] = (
+    100 * compare_props['Random'] / compare_props['Overall'] - 100
+)
+compare_props['Strat. %error'] = (
+    100 * compare_props['Stratified'] / compare_props['Overall'] - 100
+)
 
 for set_ in (strat_train_set, strat_test_set):
     set_.drop('income_cat', axis=1, inplace=True)
@@ -99,14 +103,17 @@ imputer.fit(housing_num)
 X = imputer.transform(housing_num)
 
 housing_tr = pd.DataFrame(
-    X, columns=housing_num.columns,
+    X,
+    columns=housing_num.columns,
     index=housing.index,
 )
 housing_tr['rooms_per_household'] = housing_tr['total_rooms'] / housing_tr['households']
-housing_tr['bedrooms_per_room'] = housing_tr['total_bedrooms'] / \
-    housing_tr['total_rooms']
-housing_tr['population_per_household'] = housing_tr['population'] / \
-    housing_tr['households']
+housing_tr['bedrooms_per_room'] = (
+    housing_tr['total_bedrooms'] / housing_tr['total_rooms']
+)
+housing_tr['population_per_household'] = (
+    housing_tr['population'] / housing_tr['households']
+)
 
 housing_cat = housing[['ocean_proximity']]
 housing_prepared = housing_tr.join(pd.get_dummies(housing_cat, drop_first=True))
@@ -142,8 +149,12 @@ param_distribs = {
 
 forest_reg = RandomForestRegressor(random_state=42)
 rnd_search = RandomizedSearchCV(
-    forest_reg, param_distributions=param_distribs,
-    n_iter=10, cv=5, scoring='neg_mean_squared_error', random_state=42,
+    forest_reg,
+    param_distributions=param_distribs,
+    n_iter=10,
+    cv=5,
+    scoring='neg_mean_squared_error',
+    random_state=42,
 )
 rnd_search.fit(housing_prepared, housing_labels)
 cvres = rnd_search.cv_results_
@@ -161,8 +172,11 @@ param_grid = [
 forest_reg = RandomForestRegressor(random_state=42)
 # train across 5 folds, that's a total of (12+6)*5=90 rounds of training
 grid_search = GridSearchCV(
-    forest_reg, param_grid, cv=5,
-    scoring='neg_mean_squared_error', return_train_score=True,
+    forest_reg,
+    param_grid,
+    cv=5,
+    scoring='neg_mean_squared_error',
+    return_train_score=True,
 )
 grid_search.fit(housing_prepared, housing_labels)
 
@@ -183,15 +197,19 @@ y_test = strat_test_set['median_house_value'].copy()
 X_test_num = X_test.drop('ocean_proximity', axis=1)
 X_test_prepared = imputer.transform(X_test_num)
 X_test_prepared = pd.DataFrame(
-    X_test_prepared, columns=X_test_num.columns,
+    X_test_prepared,
+    columns=X_test_num.columns,
     index=X_test.index,
 )
-X_test_prepared['rooms_per_household'] = X_test_prepared['total_rooms'] / \
-    X_test_prepared['households']
-X_test_prepared['bedrooms_per_room'] = X_test_prepared['total_bedrooms'] / \
-    X_test_prepared['total_rooms']
-X_test_prepared['population_per_household'] = X_test_prepared['population'] / \
-    X_test_prepared['households']
+X_test_prepared['rooms_per_household'] = (
+    X_test_prepared['total_rooms'] / X_test_prepared['households']
+)
+X_test_prepared['bedrooms_per_room'] = (
+    X_test_prepared['total_bedrooms'] / X_test_prepared['total_rooms']
+)
+X_test_prepared['population_per_household'] = (
+    X_test_prepared['population'] / X_test_prepared['households']
+)
 
 X_test_cat = X_test[['ocean_proximity']]
 X_test_prepared = X_test_prepared.join(pd.get_dummies(X_test_cat, drop_first=True))
