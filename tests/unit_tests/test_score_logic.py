c187e68a58393560a1a09b28af0f3dce364f288e
Danasekarane Arounachalam - TA <danasekarane.aro@tigeranalytics.com>
2024-05-02 11:49:17 +0530

    #15: Adding local install package for the module and tesing it

---
+++ tests/unit_tests/test_score_logic.py
@@ -0,0 +1,23 @@
+import unittest
+
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
+from housePricePrediction import scoring_logic
+
+
+class TestScoringModel(unittest.TestCase):
+    """ Test class for Scoring Model class
+    """
+    def test_scoring_logic(self):
+        """ Test the scoring logic"""
+        self.y = np.array([2, 3, 4, 5])
+        self.y_pred = np.array([2.5, 4, 5, 4.5])
+        rmse_exp = np.sqrt(mean_squared_error(self.y, self.y_pred))
+        rmse_act, _ = scoring_logic.scoring_logic(self.y, self.y_pred)
+        self.assertIsInstance(rmse_act, float)
+        self.assertAlmostEqual(rmse_act, rmse_exp, places=4)
+
+
+if __name__ == '__main__':
+    unittest.main()
